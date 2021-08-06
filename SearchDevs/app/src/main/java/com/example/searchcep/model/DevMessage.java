121f3c0e631b3b18addd4f5820eda459a09c2114
Mirella <mirella.lds@gmail.com>
2021-08-06 18:28:23 -0300

    error fix

--- SearchDevs/app/src/main/java/com/example/searchcep/model/DevMessage.java
+++ SearchDevs/app/src/main/java/com/example/searchcep/model/DevMessage.java
@@ -1,6 +1,10 @@
 package com.example.searchcep.model;
 
 public class DevMessage {
+    public DevMessage(String menssagem) {
+        this.menssagem = menssagem;
+    }
+
     public String getMenssagem() {
         return menssagem;
     }
--- SearchDevs/app/src/main/java/com/example/searchcep/repository/DevRepo.java
+++ SearchDevs/app/src/main/java/com/example/searchcep/repository/DevRepo.java
@@ -3,6 +3,8 @@ package com.example.searchcep.repository;
 import android.os.Messenger;
 
 import com.example.searchcep.model.Dev;
+import com.example.searchcep.model.DevMessage;
+import com.example.searchcep.service.RetrofitConfig;
 
 import java.util.List;
 
@@ -14,18 +16,18 @@ public class DevRepo {
 
     public interface ResultListener {
         void sucesso(List<Dev> dev);
-        void falha(Messenger messenger);
+        void falha(DevMessage messenger);
     }
 
     public void getDevRepo(final ResultListener resultadoDaRequisicao){
-        Call<List<Dev>> call = new ConfigRouter().getAllDevs().getAllGuitHubDevs();
+        Call<List<Dev>> call = new RetrofitConfig().getDevService().getAllDevs();
 
         call.enqueue(new Callback<List<Dev>>() {
             @Override
             public void onResponse(Call<List<Dev>> call, Response<List<Dev>> response) {
                 if(!response.isSuccessful()){
                     int code = response.code();
-                    resultadoDaRequisicao.falha(new Messenger(code, response.errorBody().toString()));
+                    resultadoDaRequisicao.falha(new DevMessage(response.errorBody().toString()));
                 } else {
                     List<Dev> list = response.body();
                     resultadoDaRequisicao.sucesso(list);
@@ -35,7 +37,7 @@ public class DevRepo {
             @Override
             public void onFailure(Call<List<Dev>> call, Throwable t) {
                 // O erro 500 significa que o servidor falou em lhe responder
-                resultadoDaRequisicao.falha(new Messenger(500, "Deu erro na requisição! Error message: \n" + t.getMessage()));
+                resultadoDaRequisicao.falha(new DevMessage("Deu erro na requisição! Error message: \n" + t.getMessage()));
             }
         });
     }
